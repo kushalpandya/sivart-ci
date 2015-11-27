0741930799113da9ddc30c19244965587a8157df
Kushal Pandya <kushalpandya@outlook.com>
2015-11-28 00:28:22 +0530

    Express Server starter script.

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * sivart-ci v0.1.0
+ *
+ * A simple interactive CI Dashboard built on MEAN stack.
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 27 November, 2015
+ *
+ * Sivart Server Script.
+ */
+
+var fs = require('fs'),
+    configuration = JSON.parse(fs.readFileSync('configuration.json'));
+
+var sivartApp = require('./app');
+
+sivartApp.listen(configuration.sivart.serverPort);
+console.log("sivart-ci started on port : ", configuration.sivart.serverPort);
