3cfdbe3d54e7e7a860d00f721ab5f06c3b20e429
Kushal Pandya <kushalpandya@outlook.com>
2015-11-27 23:41:29 +0530

    REST Routes added.

--- server.js
+++ server.js
@@ -14,8 +14,7 @@
  */
 var fs = require('fs'),
     stringTemplate = require('string-template'),
-    configuration = JSON.parse(fs.readFileSync('configuration.json')),
-    sivartApp;
+    configuration = JSON.parse(fs.readFileSync('configuration.json'));
 
 /*
  * Load Modules.
@@ -27,10 +26,21 @@ var express = require('express'),
     morgan = require('morgan');
 
 /*
- * Intialize App Server.
+ * Declare App Globals
  */
+var sivartApp,
+    changelistSchema,
+    ChangelistModel,
+    socket,
+    http;
 
 sivartApp = express();
+http = require('http').Server(sivartApp);
+socket = require('socket.io')(http);
+
+/*
+ * Intialize App Server.
+ */
 
 mongoose.connect(stringTemplate('{protocol}://{dbUserName}:{dbPassword}@{dbURL}', configuration.modulus));
 
@@ -41,5 +51,100 @@ sivartApp.use(bodyParser.json());
 sivartApp.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 sivartApp.use(methodOverride());
 
+/*
+ * Start Server
+ */
 sivartApp.listen(configuration.sivart.serverPort);
 console.log("sivart-ci started on port : ", configuration.sivart.serverPort);
+
+/*
+ * Create Models
+ */
+changelistSchema = new mongoose.Schema({
+    changeListName: String,
+    owner: String,
+    timeStarted: Number,
+    activity: {
+        status: Number,
+        phase: Number
+    },
+    build: {
+        timeCompleted: Number
+    },
+    unitTest: {
+        total: Number,
+        passCount: Number,
+        duration: Number
+    },
+    functionalTest: {
+        total: Number,
+        passCount: Number,
+        duration: Number
+    }
+});
+
+ChangelistModel = mongoose.model('ChangelistModel', changelistSchema);
+
+/*
+ * Create REST API.
+ */
+
+// @GET
+// Gets all Changelists available.
+sivartApp.get('/api/changelists', function(req, res) {
+    ChangelistModel.find(function(err, changelists) {
+        if (err)
+            res.send(err);
+
+        res.json(changelists);
+    });
+});
+
+// @GET
+// Gets Changelist by ID.
+sivartApp.get('/api/changelist/:changeListId', function(req, res) {
+    ChangelistModel.findById(req.params.changeListId, function(err, changelist) {
+        if (err)
+            res.send(err);
+
+        res.json(changelist);
+    });
+});
+
+// @POST
+// Create a Changelist.
+sivartApp.post('/api/changelist', function(req, res) {
+    var newChangelist = req.body;
+
+    ChangelistModel.create(newChangelist, function(err, changelist) {
+        if (err)
+            res.send(err);
+        else
+            console.log('Created Changelist with name ', changelist.changeListName);
+
+        res.json({
+            status: 1,
+            id: changelist.id,
+            changeListName: changelist.changeListName
+        });
+    });
+});
+
+// @DELETE
+// Delete a Changelist by ID.
+sivartApp.delete('/api/changelist/:changeListId', function(req, res) {
+    var changeListId = {
+        _id: req.params.changeListId
+    };
+
+    ChangelistModel.remove(changeListId, function(err, changelist) {
+        if (err)
+            res.send(err);
+        else
+            console.log('Changelist Deleted with ID ', changeListId._id);
+
+        res.json({
+            status: 1
+        });
+    });
+});
