fda69ec3f4c5472a3c05bf98d8677bbf4267176e
Kushal Pandya <kushalpandya@outlook.com>
2015-11-27 12:52:44 +0530

    sivart-ci server script.

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * sivart-ci v0.1.0
+ *
+ * A simple interactive CI Dashboard built on MEAN stack.
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 27 November, 2015
+ *
+ * Sivart Server Script.
+ */
