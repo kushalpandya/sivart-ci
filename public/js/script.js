3a13f3a5d2c3f76dea5e867584421d17acd1a590
Kushal Pandya <kushalpandya@outlook.com>
2015-11-28 19:02:39 +0530

    Sivart Main Script.

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * sivart-ci v0.1.0
+ *
+ * A simple interactive CI Dashboard built on MEAN stack.
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 28 November, 2015
+ *
+ * Sivart Main Script.
+ */
+
+$(function() {
+    alert('Dom ready');
+})();
